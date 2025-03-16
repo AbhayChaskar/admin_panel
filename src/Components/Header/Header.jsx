6aa5fb14e82c8c995bf090eac64986c9744ad0bf
AbhayChaskar <abhayrc.521@gmail.com>
2025-03-16 19:09:51 +0530

    added styled searchbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Header.css";
 import Logo from "../Logo/Logo";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Header = () => {
   return (
     <div id="header" className="header fixed-top d-flex align-items-center">
       <Logo/>
+      <SearchBar/>
     </div>
   );
 };
---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './SearchBar.css'
+
+const SearchBar = () => {
+  return (
+    <div className='search-bar'>
+        <form className='search-form d-flex align-items-center' method='POST' action="#">
+            <input type='text' name='query' placeholder='Search' className='Enter Keywords' />
+            <button type='submit' title="Search">
+                <i className='bi bi-search'></i>
+            </button>
+        </form>
+    </div>
+  )
+}
+
+export default SearchBar
