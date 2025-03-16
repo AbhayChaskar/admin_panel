a705af8ca7e37690644643502b7d56eb371b7088
AbhayChaskar <abhayrc.521@gmail.com>
2025-03-16 18:54:06 +0530

    Added toggle sidebar with css

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Logo.css";
 
 const Logo = () => {
+  const [isSidebarVisible, setSidebarVisible] = useState(false);
+
+  const handleToggleSidebar = () => {
+    setSidebarVisible(true);
+  };
+
   return (
-    <div id="logo" className="logo">
-      Logo
+    <div className="d-flex align-items-center justify-content-between">
+      <a href="/" className="logo d-flex align-items-center">
+        <span className="d-none d-lg-block">Admin Panel</span>
+      </a>
+      <i className={`bi bi-list toggle-sidebar-btn ${isSidebarVisible ? "show-sidebar" : ""}`} onClick={handleToggleSidebar}> </i>
     </div>
   );
 };
