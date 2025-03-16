7662a455ee0a8d012e127f665f59f3e88a135a66
AbhayChaskar <abhayrc.521@gmail.com>
2025-03-17 00:24:17 +0530

    Navbar components

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Header.css";
 import Logo from "../Logo/Logo";
 import SearchBar from "../SearchBar/SearchBar";
+import Navbar from "../Navbar/Navbar";
 
 const Header = () => {
   return (
     <div id="header" className="header fixed-top d-flex align-items-center">
       <Logo/>
       <SearchBar/>
+      <Navbar/>
     </div>
   );
 };
