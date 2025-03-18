34c65b40393b08037178e1a06d955d7eb58837ec
AbhayChaskar <abhayrc.521@gmail.com>
2025-03-19 00:04:09 +0530

    Sidebar div

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import './App.css'
 import Header from './Components/Header/Header'
+import Sidebar from './Components/Sidebar/Sidebar'
 
 function App() {
   return (
     <>
       <div>
         <Header/>
+        <Sidebar/>
       </div>
     </>
   )
