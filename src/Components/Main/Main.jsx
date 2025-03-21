1bb51ee00f192237f1e8f568c59fe0fbf490c0c7
AbhayChaskar <abhayrc.521@gmail.com>
2025-03-21 14:08:47 +0530

    Dashboard component

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Main.css";
 import DashboardTitle from "../DashboardTitle/DashboardTitle";
+import Dashboard from "../Dashboard/Dashboard";
 
 const Main = () => {
   return (
     <main id="main" className="main">
       <DashboardTitle page="Dashboard"/>
+      <Dashboard/>
     </main>
   );
 };
