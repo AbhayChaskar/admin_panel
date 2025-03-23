5248d445d0192371760261b24b23c9c5bc2eff3b
AbhayChaskar <abhayrc.521@gmail.com>
2025-03-24 00:23:32 +0530

    added filter for Reports

--- src/Components/Reports/Reports.jsx
+++ src/Components/Reports/Reports.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Reports.css";
+import CardFilter from "../Card/CardFilter";
 
 const Reports = () => {
-  return <div>Reports</div>;
+  const [filter, setFilter] = useState("Today");
+  const handleFilterChange = (filter) => {
+    setFilter(filter);
+  };
+  return (
+    <div className="card">
+      <CardFilter filterChange={handleFilterChange} />
+      <div className="card-body">
+        <h5 className="card-title">
+          Reports <span>| {filter}</span>
+        </h5>
+      </div>
+    </div>
+  );
 };
 
 export default Reports;
