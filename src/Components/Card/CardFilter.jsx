da4f71565501655cdb2f7211e384837633f8579b
AbhayChaskar <abhayrc.521@gmail.com>
2025-03-25 00:05:54 +0530

    Styling fix for report and cards

--- src/Components/Card/CardFilter.jsx
+++ src/Components/Card/CardFilter.jsx
@@ -7,9 +7,6 @@ const CardFilter = ({ filterChange }) => {
         <i className="bi bi-three-dots"></i>
       </a>
       <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-        <li className="dropdown-header text-start">
-          <h6>Filter</h6>
-        </li>
         <li>
           <a className="dropdown-item" onClick={() => filterChange("Today")}>
             Today
