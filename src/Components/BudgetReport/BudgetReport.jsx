882cc9f642d983934158857ebd211a677fe31d84
AbhayChaskar <abhayrc.521@gmail.com>
2025-03-31 19:13:34 +0530

    Budget charts added in report

--- src/Components/BudgetReport/BudgetReport.jsx
+++ src/Components/BudgetReport/BudgetReport.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import CardFilter from "../Card/CardFilter";
+import BudgetCharts from "./BudgetCharts";
 
 const BudgetReport = () => {
   const [filter, setFilter] = useState("Today");
@@ -13,6 +14,7 @@ const BudgetReport = () => {
         <h5 className="card-title">
           Budget Report <span>| {filter}</span>
         </h5>
+        <BudgetCharts/>
       </div>
     </div>
   );
