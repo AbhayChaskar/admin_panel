2571eb62338b36c325903fed5d1bc7e8880a6758
AbhayChaskar <abhayrc.521@gmail.com>
2025-04-06 00:19:24 +0530

    Items mapping in news

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import CardFilter from "../Card/CardFilter";
+import NewsPostItem from "./NewsPostItem";
+import Info from "../../../Api/info.json";
 
 const News = () => {
   const [filter, setFilter] = useState("Today");
@@ -11,8 +13,13 @@ const News = () => {
       <CardFilter filterChange={handleFilterChange} />
       <div className="card-body pb-0">
         <h5 className="card-title">
-          News <span>| {filter}</span>
+          News &amp; Updates <span>| {filter}</span>
         </h5>
+        <div className="news">
+          {Info.news?.map((item) => (
+            <NewsPostItem key={item.id} item={item} />
+          ))}
+        </div>
       </div>
     </div>
   );
