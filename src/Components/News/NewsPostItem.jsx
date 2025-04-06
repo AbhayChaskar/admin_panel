b12da54ca77fea957897be86035b2774797a635e
AbhayChaskar <abhayrc.521@gmail.com>
2025-04-07 00:04:31 +0530

    Styling for news items

--- src/Components/News/NewsPostItem.jsx
+++ src/Components/News/NewsPostItem.jsx
@@ -4,11 +4,11 @@ import "./News.css";
 const NewsPostItem = ({ item }) => {
   return (
     <div className="post-item clearfix">
-      <img src={item.img} alt="" />
-      <h4>
-        <a href="#">{item.title}</a>
+      <img className="news_image" src={item.img} alt="" />
+      <h4 className="news_title">
+        <a className="news_href" href="#">{item.title}</a>
       </h4>
-      <p>{item.subtitle}</p>
+      <p className="news_subtitle">{item.subtitle}</p>
     </div>
   );
 };
