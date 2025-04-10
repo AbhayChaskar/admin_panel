9f1725371f4638d7753c25dc91b7d7493f77a18b
AbhayChaskar <abhayrc.521@gmail.com>
2025-04-10 18:10:43 +0530

    scroll up functionality

--- src/Components/ScrollToTop/ScrollToTop.jsx
+++ src/Components/ScrollToTop/ScrollToTop.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import './ScrollToTop.css'
+import React, { useState, useEffect } from "react";
+import "./ScrollToTop.css";
 
 const ScrollToTop = () => {
-  return (
-    <div>ScrollToTop</div>
-  )
-}
+  const [scroll, setScroll] = useState(0);
 
-export default ScrollToTop
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY);
+    });
+    return () => {
+      window.removeEventListener("scroll", () => {
+        setScroll(window.scrollY);
+      });
+    };
+  }, [scroll]);
+
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <a
+      onClick={scrollToTop}
+      className={`scroll-to-top d-flex align-items-center justify-content-center ${
+        scroll > 100 ? "active" : undefined
+      }`}
+    >
+      <i className="bi bi-arrow-up-short"></i>
+    </a>
+  );
+};
+
+export default ScrollToTop;
